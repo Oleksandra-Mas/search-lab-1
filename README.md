49dd4ec782bc94796b1acf96d0888ad7fcbeea46
Oleksandra-Mas <sashamasyuk0702@gmail.com>
2023-03-17 22:23:25 +0100

    added fisrt part of lab

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # search-lab-1
+
+## part 1
+
+### text example
+
+I have an apple, an orange, a banana. I have an apple, a pear, a kiwi. I have a pear, a banana, a
+grape. I have an a banana, a grape, a kiwi. I have an apple, an orange, a kiwi.
+
+### query example
+
+apple OR (pear AND NOT kiwi) OR orange (apple AND orange) OR (pear AND NOT kiwi) OR orange (pear AND
+kiwi) OR (pear AND banana) OR NOT NOT kiwi
--- index.js
+++ index.js
@@ -7,105 +7,250 @@ const searchForm = document.getElementById('search-form');
 const termResult = document.getElementById('term-result');
 const documentResult = document.getElementById('document-result');
 const searchResult = document.getElementById('search-result');
+const searchqueryResult = document.getElementById('search-query');
 const termSection = document.getElementById('terms-section');
 const documentSection = document.getElementById('documents-section');
 const searchSection = document.getElementById('search-section');
 const termResultSection = document.getElementById('terms-result-section');
 const nextStageButton = document.getElementById('document-next-stage-button');
 const documentresultSection = document.getElementById('documention-result-section');
+const searchResultSection = document.getElementById('search-result-section');
 
-documentSection.style.display="none";
-searchSection.style.display="none";
-documentresultSection.style.display="none";
+documentSection.style.display = 'none';
+searchSection.style.display = 'none';
+documentresultSection.style.display = 'none';
 // ____________________________________________________
 let terms;
-let documents=[];
-let currentTermId =1;
-let currentDocId = 1;
+let documents = [];
+let documentsObject;
+let currentDocId = 0;
 
-const onTermFormSubmit = (event) => {
-    event.preventDefault();
-    const text = termInput.value;
-    if(!text){
-        alert('Введіть хоча б один терм')
-        return;
-    }
-    terms = text.split(', ').map(line=>{
-        const result ={id:currentTermId, term:line};
-        currentTermId+=1;
-        return result;
-    });
-    
-    const termsToDisplay = terms.map(term=>`term ${term.id}: "${term.term}"`).join('<br>');
-    termSection.style.display="none";
-    termResultSection.style.display="block";
-    document.getElementById('terms-result').innerHTML = termsToDisplay;
-    documentSection.style.display="block";
+const onTermFormSubmit = event => {
+  event.preventDefault();
+  const text = termInput.value;
+  if (!text) {
+    alert('Введіть хоча б один терм');
+    return;
+  }
+  terms = [...new Set(text.split(', ').map(line => line.trim()))];
+  console.log(terms);
+
+  const termsToDisplay = terms.map((term, i) => `term ${i + 1}: "${term}"`).join('<br>');
+  termSection.style.display = 'none';
+  termResultSection.style.display = 'block';
+  document.getElementById('terms-result').innerHTML = termsToDisplay;
+  documentSection.style.display = 'block';
 };
 
-const onNextStageClicked = () =>{
-    console.log(documents?.length);
-    if(!documents?.length || documents?.length<1){
-        alert('Введіть хоча б один документ');
-        return
-    }
-    documentSection.style.display='none';
-    searchSection.style.display='block';
+const onNextStageClicked = () => {
+  console.log(documents?.length);
+  if (!documents?.length || documents?.length < 1) {
+    alert('Введіть хоча б один документ');
+    return;
+  }
+  documentSection.style.display = 'none';
+  searchSection.style.display = 'block';
 };
 
-const onDocumentFormSubmit = (event) => {
-    event.preventDefault();
-    const text = documentInput.value;
-    if(!text) {
-        return;
-    }
-    documentresultSection.style.display="block";
-    documents.push({id: currentDocId,text});
-    currentDocId+=1;
-    document.getElementById('documention-result').innerHTML = documents.map(doc=>`document ${doc.id}: "${doc.text}"`).join('<br>');
-    documentForm.reset();
+const onDocumentFormSubmit = event => {
+  event.preventDefault();
+  const text = documentInput.value;
+  if (!text) {
+    return;
+  }
+  documentresultSection.style.display = 'block';
+  documents.push(text);
+  document.getElementById('documention-result').innerHTML = documents
+    .map((doc, i) => `document ${i + 1}: "${doc}"`)
+    .join('<br>');
+  documentsObject = createDocumentObject(documents, terms);
+  documentForm.reset();
+};
+
+const onSearchFormSubmit = event => {
+  event.preventDefault();
+  const text = searchInput.value;
+  if (!text) {
+    alert('Введіть пошуковий запит в нормальній диз`юнктивній формі');
+    return;
+  }
+  searchResultSection.style.display = 'block';
+  if (!isDNF(text)) {
+    alert('Введіть пошуковий запит в нормальній диз`юнктивній формі');
+  }
+  searchqueryResult.innerHTML = text;
+  searchResult.innerHTML = `Документи, що відповідають результатам пошуку: ${booleanModel(
+    text,
+    documentsObject,
+  )}`;
+  searchForm.reset();
 };
 
 termForm.addEventListener('submit', onTermFormSubmit);
 nextStageButton.addEventListener('click', onNextStageClicked);
 documentForm.addEventListener('submit', onDocumentFormSubmit);
+searchForm.addEventListener('submit', onSearchFormSubmit);
 
-// Define the documents in the collection
-// const docs = [
-//     { id: 1, text: "The quick brown fox jumps over the lazy dog" },
-//     { id: 2, text: "The quick brown fox jumps over the quick dog" },
-//     { id: 3, text: "The quick red fox jumps over the lazy dog" },
-//     { id: 4, text: "The quick brown cat jumps over the lazy dog" },
-//   ];
-  
-//   // Define the index of terms in the collection
-//   const index = {
-//     the: [1, 2, 3, 4],
-//     quick: [1, 2, 3],
-//     brown: [1, 2, 4],
-//     fox: [1, 2, 3],
-//     jumps: [1, 2, 3, 4],
-//     over: [1, 2, 3, 4],
-//     lazy: [1, 3, 4],
-//     dog: [1, 2, 3, 4],
-//     red: [3],
-//     cat: [4],
-//   };
-  
-//   // Define the query
-//   const query = "quick AND brown AND fox";
-  
-//   // Tokenize the query
-//   const termss = query.split(/\s+/);
-  
-//   // Retrieve the document IDs for each term
-//   const termDocs = termss.map((term) => index[term]);
-  
-//   // Compute the intersection of document IDs for all terms
-//   const results = termDocs.reduce((acc, cur) => {
-//     return acc.filter((id) => cur.includes(id));
-//   });
-  
-//   // Print the results
-//   console.log(results);
-  
+// ______________________________________________________
+function isDNF(query) {
+  if (query.includes(') AND') || query.includes('AND (')) {
+    return false;
+  }
+  return true;
+}
+
+const getTerms = query =>
+  query
+    .split(/(\(|\)|\s+OR\s+|\s+AND\s+|\s+NOT\s+)/)
+    .map(term => term.trim())
+    .filter(term => !!term);
+
+const getSubTerms = (terms = []) => {
+  let subterms;
+  if (terms.includes('(')) {
+    subterms = [[]];
+    let subtermIndex = 0;
+    let skipIterationIdx;
+    terms.forEach((term, i) => {
+      if (i !== skipIterationIdx) {
+        if (term === ')') {
+          subtermIndex += 1;
+          subterms[subtermIndex] = [];
+          if (terms?.[i + 1]) {
+            subterms[subtermIndex].push(terms?.[i + 1]);
+            subtermIndex += 1;
+            subterms[subtermIndex] = [];
+            skipIterationIdx = i + 1;
+          }
+        }
+        if (term === '(') {
+          if (terms?.[i - 1] && terms?.[i - 2] !== ')') {
+            subterms[subtermIndex].pop();
+            subtermIndex += 1;
+            subterms[subtermIndex] = [terms?.[i - 1]];
+          }
+          subtermIndex += 1;
+          subterms[subtermIndex] = [];
+        }
+        if (term !== ')' && term !== '(') {
+          subterms[subtermIndex].push(term);
+        }
+      } else {
+        skipIterationIdx = undefined;
+      }
+    });
+  }
+  return subterms?.filter(el => el.length > 0);
+};
+
+const searchBySubterms = (terms, docs) => {
+  let result = [];
+  for (let i = 0; i < terms.length; i++) {
+    const term = terms[i];
+
+    if (term === 'AND' || term === 'OR' || term === 'NOT' || term === '(' || term === ')') {
+      continue;
+    } else if (term.startsWith('NOT ')) {
+      const notTerm = term.substring(4);
+      const notResult = [];
+
+      for (const doc in docs) {
+        if (!docs.hasOwnProperty(doc)) {
+          continue;
+        }
+        if (!docs[doc].includes(notTerm)) {
+          notResult.push(doc);
+        }
+      }
+
+      if (notResult.length > 0) {
+        if (result.length === 0) {
+          result = notResult;
+        } else if (terms[i - 1] === 'OR') {
+          result = [...new Set([...result, ...notResult])];
+        } else if (terms[i - 1] === 'AND') {
+          result = result.filter(doc => notResult.includes(doc));
+        }
+      }
+    } else {
+      const termResult = [];
+
+      for (const doc in docs) {
+        if (!docs.hasOwnProperty(doc)) {
+          continue;
+        }
+
+        if (docs[doc].includes(term)) {
+          termResult.push(doc);
+        }
+      }
+      if (termResult.length > 0) {
+        if (result.length === 0) {
+          result = termResult;
+        } else if (terms[i - 1] === 'OR') {
+          result = [...new Set([...result, ...termResult])];
+        } else if (terms[i - 1] === 'AND') {
+          result = result.filter(doc => termResult.includes(doc));
+        }
+      }
+    }
+  }
+  return result;
+};
+
+function booleanModel(query, docs) {
+  const terms = getTerms(query);
+  const subterms = getSubTerms(terms);
+
+  if (subterms?.length > 0) {
+    const subresults = subterms.map((sterms, i) => {
+      if (i % 2 === 0) {
+        return searchBySubterms(sterms, docs);
+      }
+      return sterms[0];
+    });
+    let result = [];
+    for (let i = 1; i < subresults.length - 1; i += 2) {
+      if (result.length === 0) {
+        if (i === 1) {
+          if (subresults[i] === 'OR') {
+            result = [...subresults[i - 1], ...subresults[i + 1]];
+          } else if (subresults[i] === 'AND') {
+            result = subresults[i - 1].filter(doc => subresults[i + 1].includes(doc));
+          }
+        } else {
+          if (subresults[i] === 'OR') {
+            result = [...result, ...subresults[i + 1]];
+          } else if (subresults[i] === 'AND') {
+            result = result.filter(doc => subresults[i + 1].includes(doc));
+          }
+        }
+      } else if (result.length > 0) {
+        if (subresults[i] === 'OR') {
+          result = [...result, ...subresults[i + 1]];
+        } else if (subresults[i] === 'AND') {
+          result = result.filter(doc => subresults[i + 1].includes(doc));
+        }
+      }
+    }
+    return [...new Set(result)];
+  }
+
+  return searchBySubterms(terms);
+}
+
+function createDocumentObject(documents, terms) {
+  const docs = {};
+  for (let i = 0; i < documents.length; i++) {
+    const doc = documents[i];
+    const docTerms = [];
+    for (let j = 0; j < terms.length; j++) {
+      const term = terms[j];
+      if (doc.includes(term)) {
+        docTerms.push(term);
+      }
+    }
+    docs[i + 1] = docTerms;
+  }
+  return docs;
+}
